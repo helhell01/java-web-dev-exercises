cd2212e0117e857632f2dde3589b6e883a6e632c
Hanna Luechtefeld <hannaluechtefeld@gmail.com>
2023-07-05 16:03:08 -0500

    fun with quizzes studio + spinning discs studio

---
+++ src/org/launchcode/java/studios/spinningDiscs/OpticalDisc.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.studios.spinningDiscs;
+
+public interface OpticalDisc {
+
+    // These methods are only suggestions.
+    void spinDisc();
+    void readData();
+
+}
+
